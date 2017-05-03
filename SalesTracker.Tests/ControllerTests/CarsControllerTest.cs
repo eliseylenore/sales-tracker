f1ff510b43f661d5ffc1dcfd5782d77fcd41105e
Elise St Hilaire and Shruti Priya <elise+shruti@>
2017-05-03 10:53:58 -0700

    passes controller test

---
+++ SalesTracker.Tests/ControllerTests/CarsControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesTracker.Controllers;
+using Xunit;
+using SalesTracker.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SalesTracker.Tests
+{
+    public class CarsControllerTest
+    {
+        [Fact]
+        public void Get_ViewResult_Index_Test()
+        {
+            //Arrange
+            CarsController controller = new CarsController();
+
+            //Act
+            var result = controller.Index();
+
+            //Assert
+            Assert.IsType<ViewResult>(result);
+
+        }
+    }
+}
